3cd9ebd6279ade47155f4a09806594500cf9edcf
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-01-10 14:24:19 +0100

    use index to find all annotation with step definition
    We are going to remove NotIndexedCucumberExtension in 2020.1 release. This PR will help to make the plugin compatible with the upcoming release of IDEA. Also using index instead of scanning each .dart file should improve performance of the plugin.

--- src/cd/connect/idea/plugins/cucumber/dart/CucumberDartNIExtension.java
+++ src/cd/connect/idea/plugins/cucumber/dart/CucumberDartNIExtension.java
@@ -1,104 +1,32 @@
 package cd.connect.idea.plugins.cucumber.dart;
 
+import cd.connect.idea.plugins.cucumber.dart.steps.DartAnnotatedStepDefinition;
 import cd.connect.idea.plugins.cucumber.dart.steps.DartStepDefinitionCreator;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.indexing.FileBasedIndex;
 import com.jetbrains.lang.dart.DartFileType;
-import com.jetbrains.lang.dart.projectWizard.DartModuleBuilder;
-import com.jetbrains.lang.dart.psi.DartClassDefinition;
-import com.jetbrains.lang.dart.psi.DartClassMembers;
 import com.jetbrains.lang.dart.psi.DartFile;
+import com.jetbrains.lang.dart.psi.DartMethodDeclaration;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.BDDFrameworkType;
 import org.jetbrains.plugins.cucumber.StepDefinitionCreator;
-import cd.connect.idea.plugins.cucumber.dart.steps.DartAnnotatedStepDefinition;
 import org.jetbrains.plugins.cucumber.psi.GherkinFile;
+import org.jetbrains.plugins.cucumber.steps.AbstractCucumberExtension;
 import org.jetbrains.plugins.cucumber.steps.AbstractStepDefinition;
-import org.jetbrains.plugins.cucumber.steps.NotIndexedCucumberExtension;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-public class CucumberDartNIExtension extends NotIndexedCucumberExtension {
-  @Override
-  protected void loadStepDefinitionRootsFromLibraries(Module module, List<PsiDirectory> roots, Set<String> directories) {
-  }
-
-  @Override
-  protected Collection<AbstractStepDefinition> getStepDefinitions(@NotNull PsiFile psiFile) {
-    final List<AbstractStepDefinition> newDefs = new ArrayList<>();
-    if (psiFile instanceof DartFile) {
-      final DartClassDefinition clazz = PsiTreeUtil.getChildOfType(psiFile, DartClassDefinition.class);
-
-      if (clazz != null && clazz.getClassBody() != null && clazz.getClassBody().getClassMembers() != null) {
-        DartClassMembers cMembers = clazz.getClassBody().getClassMembers();
-        cMembers.getMethodDeclarationList().forEach(member -> {
-          String annotation = CucumberDartUtil.findDartCucumberAnnotation(member);
-
-          if (annotation != null) {
-            newDefs.add(new DartAnnotatedStepDefinition(member, annotation));
-          }
-        });
-      }
-    }
-    return newDefs;
-  }
-
-  @Override
-  protected void collectAllStepDefsProviders(@NotNull List<VirtualFile> providers, @NotNull Project project) {
-    final Module[] modules = ModuleManager.getInstance(project).getModules();
-    final DartModuleBuilder dmb = new DartModuleBuilder();
-    ModuleType moduleType = dmb.getModuleType();
-    for (Module module : modules) {
-      if (ModuleType.get(module).equals(moduleType)) {
-        final VirtualFile[] roots = ModuleRootManager.getInstance(module).getContentRoots();
-        ContainerUtil.addAll(providers, roots);
-      }
-    }
-
-  }
-
-  @Override
-  public void findRelatedStepDefsRoots(@NotNull final Module module, @NotNull final PsiFile featureFile,
-                                       List<PsiDirectory> newStepDefinitionsRoots, Set<String> processedStepDirectories) {
-    // ToDo: check if inside test folder
-    PsiDirectory currDir = featureFile.getContainingDirectory();
-    PsiDirectory featureFileDirectory = currDir;
-
-    while (currDir != null && currDir.isDirectory() && (!currDir.getName().equals("test_driver") || currDir.getName().equals("test"))) {
-      if ("steps".equals(currDir.getName())) {
-        if (!processedStepDirectories.contains(currDir.getVirtualFile().getPath())) {
-          newStepDefinitionsRoots.add(currDir);
-        }
-      }
-
-      Arrays.stream(currDir.getSubdirectories()).filter(d -> "steps".equals(d.getName())).findFirst().ifPresent(d -> {
-        if (!processedStepDirectories.contains(d.getVirtualFile().getPath())) {
-          newStepDefinitionsRoots.add(d);
-        }
-      });
-
-      currDir = currDir.getParentDirectory();
-    }
-
-    if (currDir != null && !processedStepDirectories.contains(currDir.getVirtualFile().getPath())) {
-      newStepDefinitionsRoots.add(currDir);
-    }
-  }
-
+public class CucumberDartNIExtension extends AbstractCucumberExtension {
   @Override
   public boolean isStepLikeFile(@NotNull PsiElement child, @NotNull PsiElement parent) {
     return child instanceof DartFile && ((DartFile)child).getName().endsWith(".dart");
@@ -119,5 +47,58 @@ public class CucumberDartNIExtension extends NotIndexedCucumberExtension {
   @Override
   public StepDefinitionCreator getStepDefinitionCreator() {
     return new DartStepDefinitionCreator();
+  }
+
+  @Override
+  public List<AbstractStepDefinition> loadStepsFor(@Nullable PsiFile featureFile, @NotNull Module module) {
+    final List<AbstractStepDefinition> result = new ArrayList<>();
+    final FileBasedIndex fileBasedIndex = FileBasedIndex.getInstance();
+
+    Project project = module.getProject();
+    fileBasedIndex.processValues(DartCucumberIndex.INDEX_ID, true, null,
+            (file, value) -> {
+              ProgressManager.checkCanceled();
+
+              PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+              if (psiFile == null) {
+                return true;
+              }
+
+              for (Integer offset : value) {
+                PsiElement element = psiFile.findElementAt(offset + 1);
+                DartMethodDeclaration member = PsiTreeUtil.getParentOfType(element, DartMethodDeclaration.class);
+                if (member == null) {
+                  continue;
+                }
+                String annotation = CucumberDartUtil.findDartCucumberAnnotation(member);
+                if (annotation != null) {
+                  result.add(new DartAnnotatedStepDefinition(member, annotation));
+                }
+              }
+              return true;
+            }, GlobalSearchScope.projectScope(project));
+    return result;
+  }
+
+  @Override
+  public Collection<? extends PsiFile> getStepDefinitionContainers(@NotNull GherkinFile featureFile) {
+    final Module module = ModuleUtilCore.findModuleForPsiElement(featureFile);
+    if (module == null) {
+      return Collections.emptySet();
+    }
+    List<AbstractStepDefinition> stepDefs = loadStepsFor(featureFile, module);
+
+    Set<PsiFile> result = new HashSet<>();
+    for (AbstractStepDefinition stepDef : stepDefs) {
+      PsiElement stepDefElement = stepDef.getElement();
+      if (stepDefElement != null) {
+        final PsiFile psiFile = stepDefElement.getContainingFile();
+        PsiDirectory psiDirectory = psiFile.getParent();
+        if (psiDirectory != null && isWritableStepLikeFile(psiFile, psiDirectory)) {
+          result.add(psiFile);
+        }
+      }
+    }
+    return result;
   }
 }
