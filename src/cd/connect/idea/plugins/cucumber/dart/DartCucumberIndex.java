ad64960ba0b0f75468d1734981188e0379e45db0
Richard Vowles <richard@bluetrainsoftware.com>
2020-04-23 19:35:46 +1200

    release to 1.1.5 to fix clash with groovy cucumber

--- src/cd/connect/idea/plugins/cucumber/dart/DartCucumberIndex.java
+++ src/cd/connect/idea/plugins/cucumber/dart/DartCucumberIndex.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DartCucumberIndex extends CucumberStepIndex {
-    public static final ID<Boolean, List<Integer>> INDEX_ID = ID.create("groovy.cucumber.step");
+    public static final ID<Boolean, List<Integer>> INDEX_ID = ID.create("dart.cucumber.step");
 
     @Override
     protected String[] getPackagesToScan() {
